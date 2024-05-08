205732086f065f5d605c51d20eaa86df3aa03b8a
RyuJiye <coomicuma@naver.com>
2024-05-08 17:48:13 +0900

    M,H Area 수정
    조건문에 Tag 추가

--- Assets/Scripts/InGame/HourArea.cs
+++ Assets/Scripts/InGame/HourArea.cs
@@ -9,16 +9,17 @@ public class HourArea : MonoBehaviour
     {
         gameObject.stateType = StateType.Stop;
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Object"))
+        if (collision.CompareTag("Object") || collision.CompareTag("Block") || collision.CompareTag("MovingPlatform") || collision.CompareTag("Laser") || collision.CompareTag("Water"))
         {
             triggeredObject.Add(collision.gameObject);
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Object"))
+        if (collision.CompareTag("Object") || collision.CompareTag("Block") || collision.CompareTag("MovingPlatform") || collision.CompareTag("Laser") || collision.CompareTag("Water"))
         {
             triggeredObject.Remove(collision.gameObject);
         }
--- Assets/Scripts/InGame/MinuteArea.cs
+++ Assets/Scripts/InGame/MinuteArea.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MinuteArea : MonoBehaviour
 {
@@ -9,10 +10,12 @@ public class MinuteArea : MonoBehaviour
     float nearestDistance;
     public GameObject nearestObject;
 
+
     public void ChangeStrategy(IChangable gameObject)
     {
         gameObject.stateType = StateType.Flow;
     }
+
     private void Awake()
     {
         nearestDistance = Mathf.Infinity;
@@ -20,20 +23,29 @@ public class MinuteArea : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Object"))
+        if (collision.CompareTag("Object")|| collision.CompareTag("Block")|| collision.CompareTag("MovingPlatform")|| collision.CompareTag("Laser")|| collision.CompareTag("Water"))
         {
-            triggeredObjectRigid.Add(collision.GetComponent<Rigidbody2D>());
+            //충돌한 오브젝트의 상태를 가져옴
+            IChangable changableObject = collision.GetComponent<IChangable>();
+
+            //상태가 Stop인 경우에만 리스트에 추가
+            if (changableObject != null && changableObject.stateType == StateType.Stop)
+            {
+                triggeredObjectRigid.Add(collision.GetComponent<Rigidbody2D>());
+            }
+           
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Object"))
+        if (collision.CompareTag("Object") || collision.CompareTag("Block") || collision.CompareTag("MovingPlatform") || collision.CompareTag("Laser") || collision.CompareTag("Water"))
         {
             triggeredObjectRigid.Remove(collision.GetComponent<Rigidbody2D>());
             if(collision.gameObject == nearestObject)
             {
                 nearestObject = null;
                 nearestDistance = Mathf.Infinity;
+                nearestObject.GetComponent<SpriteRenderer>().color = Color.gray;
             }
         }
     }
@@ -41,18 +53,24 @@ public class MinuteArea : MonoBehaviour
     private void OnTriggerStay2D(Collider2D collision)
     {
         foreach (Rigidbody2D rigidbody2d in triggeredObjectRigid)
-        {
+        {  
             if ((playerPosition - rigidbody2d.ClosestPoint(playerPosition)).sqrMagnitude < nearestDistance) // 플레이어와 가장 가까운 collider의 지점과 가장 가까운 부분을 비교
             {
                 nearestObject = rigidbody2d.gameObject;
                 //Debug.Log(nearestObject);
-                //노란 박스 키고 끄는 부분
+                
             }
             nearestDistance = (playerPosition - nearestObject.GetComponent<Rigidbody2D>().ClosestPoint(playerPosition)).sqrMagnitude;
         }
+        //노란 박스 키고 끄는 부분, 임시로 노란색으로 변하는걸로 구현 
+        nearestObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+
     }
+
     private void Update()
     {
         playerPosition = gameObject.transform.parent.position;
     }
+
+ 
 }
