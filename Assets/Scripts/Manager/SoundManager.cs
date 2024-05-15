1b05200addd573a16210966596632b156f42039a
Sunwhi <sunwhi123@naver.com>
2024-05-15 17:06:17 +0900

    옵션창 Bgm슬라이더 구현
    메인씬, 인게임 내 옵션 창 Bgm 슬라이더 음량 조절 기능 구현 / 씬 이동해도 사운드 크기 그대로 유지할 수 있음

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -1,48 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
-    //게임매니저의 인스턴스를 담는 전역변수(static 변수이지만 이해하기 쉽게 전역변수라고 하겠다).
-    //이 게임 내에서 게임매니저 인스턴스는 이 instance에 담긴 녀석만 존재하게 할 것이다.
-    //보안을 위해 private으로.
     private static SoundManager instance = null;
-
-    void Awake()
-    {
-        if (null == instance)
-        {
-            //이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
-            instance = this;
-
-            //씬 전환이 되더라도 파괴되지 않게 한다.
-            //gameObject만으로도 이 스크립트가 컴포넌트로서 붙어있는 Hierarchy상의 게임오브젝트라는 뜻이지만, 
-            //나는 헷갈림 방지를 위해 this를 붙여주기도 한다.
-            DontDestroyOnLoad(this.gameObject);
-
-        }
-        else
-        {
-            //만약 씬 이동이 되었는데 그 씬에도 Hierarchy에 GameMgr이 존재할 수도 있다.
-            //그럴 경우엔 이전 씬에서 사용하던 인스턴스를 계속 사용해주는 경우가 많은 것 같다.
-            //그래서 이미 전역변수인 instance에 인스턴스가 존재한다면 자신(새로운 씬의 GameMgr)을 삭제해준다.
-            Destroy(this.gameObject);
-        }
-    }
-
-    //게임 매니저 인스턴스에 접근할 수 있는 프로퍼티. static이므로 다른 클래스에서 맘껏 호출할 수 있다.
     public static SoundManager Instance
     {
         get
         {
-            if (null == instance)
+            if (instance == null)
             {
                 return null;
             }
             return instance;
         }
     }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
-    //AudioSource backgroundMusic = GameObject.AddComponent<AudioSource>();
+    [SerializeField] AudioClip bgmWood;
+    [SerializeField] AudioClip bgmTown;
+    private AudioSource audioSource;
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        if(bgmWood == null) 
+        {
+            bgmWood = Resources.Load<AudioClip>("Audio/Background/Wood");
+        }
+        if(bgmTown == null)
+        {
+            bgmTown = Resources.Load<AudioClip>("Audio/Background/Town");
+        }
+        audioSource.clip = bgmWood;
+        audioSource.Play();
+    }
+    void Update()
+    {
+        if( audioSource.clip != bgmWood && SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            audioSource.clip = bgmWood;
+            audioSource.Play();
+        }
+        if (audioSource.clip != bgmTown && SceneManager.GetActiveScene().name == "Stage01")
+        {
+            audioSource.clip = bgmTown;
+            audioSource.Play();
+        }
+    }
+
+    public float bgmVolume = 1.0f;// 메인화면과 인게임 옵션 bgm 슬라이드 값을 이 값으로 통일한다.
+    public float effentVolume = 1.0f; //
+    public void OnVolumeChange(float volume)
+    {
+        audioSource.volume = volume;
+        bgmVolume = volume;
+    }
+
 }
