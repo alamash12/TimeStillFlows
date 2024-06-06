aa2290981ffad8a869d351cbe75c56cf98be40fc
RyuJiye <coomicuma@naver.com>
2024-06-06 14:17:08 +0900

    MovingPlatform 이미지
    이미지 전환

--- Assets/Scripts/InGame/Objects/SpriteDefine.cs
+++ Assets/Scripts/InGame/Objects/SpriteDefine.cs
@@ -18,5 +18,15 @@ public class SpriteDefine
         LaserBodyStop,
         LaserLightFlow,
         LaserLightStop,
+        MovingPlatform_LongCenterFlow,
+        MovingPlatform_LongCenterStop,
+        MovingPlatform_LongLeftFlow,
+        MovingPlatform_LongLeftStop,
+        MovingPlatform_LongRightFlow,
+        MovingPlatform_LongRightStop,
+        MovingPlatform_Short01Flow,
+        MovingPlatform_Short01Stop,
+        MovingPlatform_Short02Flow,
+        MovingPlatform_Short02Stop,
     }
 }
