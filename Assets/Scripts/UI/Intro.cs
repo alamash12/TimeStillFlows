e43a1b586cf263cd3af309000366b84b8e07f95a
Minwoo <168280537+MyoLynn@users.noreply.github.com>
2024-06-27 20:49:37 +0900

    Revert "Merge branch 'main' of https://github.com/alamash12/TimeStillFlows"
    This reverts commit b6d74ca584c5b734996712c963fd763b4a29eecd, reversing
    changes made to 33424ed152fcfe9ec7cbfe376b4a7eb6d8ece424.

--- Assets/Scripts/UI/Intro.cs
+++ Assets/Scripts/UI/Intro.cs
@@ -1,79 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
 
-public class Intro : MonoBehaviour, IPointerDownHandler
+public class Intro : MonoBehaviour
 {
-    public Image fadeImg;
-    public float fadeSpeed = 0.8f;
-    bool isLastScene = false;
     public void SkipIntro()
     {
         SceneManager.LoadScene("Stage01");
-    }
-
-    private void Start()
-    {
-        StartCoroutine(SceneChange());
-    }
-
-    IEnumerator SceneChange()
-    {
-        int childNum = transform.childCount;
-
-        for (int i = 0; i < childNum; i++)
-        {
-            GameObject child = transform.GetChild(i).gameObject;
-            fadeImg = child.GetComponent<Image>();
-
-            child.SetActive(true);
-            yield return StartCoroutine(FadeIn());
-
-            yield return new WaitForSeconds(2.5f);
-
-            if (i != childNum - 1)
-            {
-                yield return StartCoroutine(FadeOut());
-                child.SetActive(false);
-            }
-            if(i == childNum - 1) isLastScene = true;
-        }
-    }
-
-    IEnumerator FadeIn()
-    {
-        float alpha = 0;
-        while (alpha < 1)
-        {
-            alpha += Time.deltaTime * fadeSpeed * 2;
-            fadeImg.color = new Color(fadeImg.color.r, fadeImg.color.g, fadeImg.color.b, alpha);
-            yield return null;
-        }
-    }
-    IEnumerator FadeOut()
-    {
-        float alpha = fadeImg.color.a;
-        while (alpha > 0)
-        {
-            alpha -= Time.deltaTime * fadeSpeed * 2f;
-            fadeImg.color = new Color(fadeImg.color.r, fadeImg.color.g, fadeImg.color.b, alpha);
-            yield return null;
-        }
-    }
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        if (isLastScene)
-        {
-            StartCoroutine(ExitSequence());
-        }
-    }
-    IEnumerator ExitSequence()
-    {
-        yield return StartCoroutine(FadeOut());  // FadeOut을 완료할 때까지 기다림
-        SceneManager.LoadScene("Stage01");  // 페이드 아웃 완료 후 씬 로드
     }
 }
