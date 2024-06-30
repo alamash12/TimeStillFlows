8edc759cb075caa779a0e2c14507d1eeee9e5bb5
alamash12 <alamash@naver.com>
2024-06-30 23:34:16 +0900

    점프 오류 수정, 블록 드래그 효과음 삽입

--- Assets/Scripts/InGame/Player/PlayerJump.cs
+++ Assets/Scripts/InGame/Player/PlayerJump.cs
@@ -10,6 +10,7 @@ public class PlayerJump : MonoBehaviour
     Vector2 groundCheckSize = new Vector2(1f, 0.05f); // 점프를 위한 OverlapBox 크기변수
     Rigidbody2D playerRigid;
     bool isGround = false;
+    bool isJumping = false;
     Animator animator;
     private void Awake()
     {
@@ -18,12 +19,14 @@ public class PlayerJump : MonoBehaviour
     }
     public void Jump()
     {
-        if (isGround)
+        if (isGround && !isJumping)
         {
             isGround = false;
+            isJumping = true;
             playerRigid.velocity = new Vector2(playerRigid.velocity.x, 0);
             playerRigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             animator.SetBool("isJump", true);
+            Invoke("ResetJumping", 0.1f);
         }
     }
     void Update()
@@ -40,9 +43,9 @@ public class PlayerJump : MonoBehaviour
     private void LateUpdate()
     {
         Collider2D collider = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0f);
-        if (collider != null)
+        if (collider != null && !isJumping)
         {
-            if (collider.gameObject != gameObject && playerRigid.velocity.y < -0.01f) // 자기 자신을 제외한 충돌 감지
+            if (collider.gameObject != gameObject) // 자기 자신을 제외한 충돌 감지
             {
                 JumpStateReset();
             }
@@ -51,9 +54,12 @@ public class PlayerJump : MonoBehaviour
     public void JumpStateReset()
     {
         playerRigid.gravityScale = 2f; // 스케일 조절로 인해서 lateUpdate 사용
-        SoundManager.Instance.EffectSoundOn("GrassLanding"); // 흙 착지 소리
         isGround = true;
         animator.SetBool("isJump", false);
-        Debug.Log("landing");
+    }
+
+    void ResetJumping()
+    {
+        isJumping = false; // 일정 시간 후에 다시 점프할 수 있도록 설정
     }
 }
