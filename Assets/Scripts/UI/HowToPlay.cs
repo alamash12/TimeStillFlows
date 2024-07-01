cba0bdfdc87b8b738685767cbb1fdaeacabe697e
Sunwhi <sunwhi123@naver.com>
2024-07-01 21:36:09 +0900

    스테이지 조작법 바깥에 안 눌림

--- Assets/Scripts/UI/HowToPlay.cs
+++ Assets/Scripts/UI/HowToPlay.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class HowToPlay : MonoBehaviour
 {
     [SerializeField] GameObject panel;
+    [SerializeField] GameObject backPanel;
+    private void Start()
+    {
+        Time.timeScale = 0.0f;
+    }
     public void ClosePanel()
     {
+        Time.timeScale = 1.0f;
         panel.SetActive(false);
+        backPanel.SetActive(false);
     }
 }
