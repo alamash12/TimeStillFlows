7705af5072bab156150e95d3f891910c6af7eb44
alamash12 <alamash@naver.com>
2024-07-02 18:04:40 +0900

    점프 오류 수정, 발판 오더 수정

--- Assets/Scripts/InGame/Player/PlayerJump.cs
+++ Assets/Scripts/InGame/Player/PlayerJump.cs
@@ -19,7 +19,7 @@ public class PlayerJump : MonoBehaviour
     public void Jump()
     {
         Debug.Log(isGround);
-        if (isGround)
+        if (isGround && playerRigid.velocity.y >= 0)
         {
             playerRigid.velocity = new Vector2 (0, 0);
             SoundManager.Instance.EffectSoundOn("Jump");
