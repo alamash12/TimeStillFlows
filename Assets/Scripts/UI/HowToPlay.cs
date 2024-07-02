383f8fcb05f2c11bfd48fdc091c536f6d0510bf9
Sunwhi <sunwhi123@naver.com>
2024-07-02 23:17:10 +0900

    가이드 페이지 추가 / 화살표 추가

--- Assets/Scripts/UI/HowToPlay.cs
+++ Assets/Scripts/UI/HowToPlay.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * 스테이지 1에 뜨는 가이드에 관하여
+ */
 public class HowToPlay : MonoBehaviour
 {
     [SerializeField] GameObject htpPanel;
     [SerializeField] GameObject backPanel;
+    [SerializeField] GameObject htp2Panel;
     private void Start()
     {
         Time.timeScale = 0.0f;
@@ -14,6 +17,15 @@ public class HowToPlay : MonoBehaviour
     {
         Time.timeScale = 1.0f;
         htpPanel.SetActive(false);
+        htp2Panel.SetActive(false);
         backPanel.SetActive(false);
+    }
+    public void NextBtn()
+    {
+        htp2Panel.SetActive(true);
+    }
+    public void PrevBtn()
+    {
+        htp2Panel.SetActive(false);
     }
 }
--- Assets/Scripts/UI/HowToPlayMain.cs
+++ Assets/Scripts/UI/HowToPlayMain.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * "메인화면"에서 뜨는 가이드에 관하여
+ */
 public class HowToPlayMain : MonoBehaviour
 {
     [SerializeField] GameObject HtpPanel;
-    public void OpenHtpPanel()
+    [SerializeField] GameObject Htp2Panel;
+    public void OpenHtpPanel() // 가이드 버튼에 붙음
     {
         HtpPanel.SetActive(true);
     }
-    public void CloseHtpPanel()
+    public void CloseHtpPanel() // x 버튼에 붙음
     {
         HtpPanel.SetActive(false);
+        Htp2Panel.SetActive(false);
+    }
+    public void NextBtn()
+    {
+        Htp2Panel.SetActive(true);
+    }
+    public void PrevBtn()
+    {
+        Htp2Panel.SetActive(false);
     }
 }
